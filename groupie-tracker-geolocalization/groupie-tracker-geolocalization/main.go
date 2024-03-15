a015042a245d475f8e0482e32565ffd921f9d932
UAssylbek <154306656+UAssylbek@users.noreply.github.com>
2024-03-15 20:38:24 +0600

    Add files via upload

---
+++ groupie-tracker-geolocalization/groupie-tracker-geolocalization/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"test/utils"
+)
+
+func main() {
+
+	http.HandleFunc("/", test.ArtistFunc)
+	http.HandleFunc("/artists/", test.ArtistByIDFunc) // Добавляем новый обработчик для запросов по ID артиста
+	http.Handle("/client/", http.StripPrefix("/client/", http.FileServer(http.Dir("client"))))
+	log.Println("serve start listening on port 8080")
+	errr := http.ListenAndServe("0.0.0.0:8080", nil)
+	if errr != nil {
+		log.Fatal(errr)
+	}
+}
